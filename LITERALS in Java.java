658593023c987d0e7e3df1d98887a87c9841a4a2
omguptaa1999 <53369276+omguptaa1999@users.noreply.github.com>
2021-12-08 00:26:32 +0530

    Create LITERALS in Java.java

---
+++ LITERALS in Java.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class LITERALS2 {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 45;
+        float f1 = 5.89f;
+        short s1 = 67;
+        double d1 = 5.89d;
+        long lo = 58889898L;
+        char ch = 'A';
+        boolean a = true;
+        String str = "OmGupta";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(f1);
+        System.out.println(s1);
+        System.out.println(d1);
+        System.out.println(lo);
+        System.out.println(ch);
+        System.out.println(a);
+        System.out.println(str);
+    }
+
+}
