28242d3fbda13a1f3d7b9ef2ba4de80a6c488663
omguptaa1999 <53369276+omguptaa1999@users.noreply.github.com>
2021-12-08 01:06:38 +0530

    Create Quadratic_Equation in Java.java

---
+++ Quadratic_Equation in Java.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class QUADRATIC_EQUATION_PROBLEM_7 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int c = 8;
+        int q = b*b - (4*a*c)/(2*a);
+        System.out.println(q);
+    }
+
+}
