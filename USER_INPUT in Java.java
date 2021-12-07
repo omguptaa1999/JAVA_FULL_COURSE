959f0cff9ff8084f3958e1b3801b665cc6d33aa5
omguptaa1999 <53369276+omguptaa1999@users.noreply.github.com>
2021-12-08 00:28:46 +0530

    Create USER_INPUT in Java.java

---
+++ USER_INPUT in Java.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class USER_INPUT3 {
+    public static void main(String[] args) {
+//        SUM OF TWO NUMBERS BY USER's INPUT
+        System.out.println("Taking the User Input");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number 1: ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter number 2: ");
+//        int b = sc.nextInt();
+//        int sum = a + b;
+//        System.out.println("The sum of two numbers is: ");
+//        System.out.println(sum);
+
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+
+//        This method only print the first word of a single line.
+//        String str = sc.next();
+//        System.out.println(str);
+
+//        This method is used to print a whole line as it is.
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
