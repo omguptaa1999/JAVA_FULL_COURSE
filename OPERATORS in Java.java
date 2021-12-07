0cffe62507cba85d95f2bf9d11b716703cdf4d88
omguptaa1999 <53369276+omguptaa1999@users.noreply.github.com>
2021-12-08 00:27:52 +0530

    Create OPERATORS in Java.java

---
+++ OPERATORS in Java.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class OPERATORS_5 {
+    public static void main(String[] args) {
+        int a = 4;
+
+//        ARITHMATIC OPERATORS
+//        int b = 6%a;
+//        int b1 = 6+a;
+//        int b2 = 6-a;
+//        int b3 = 6*a;
+//        int b4 = 9;
+//        b4 += 3; // ASSIGNMENT OPERATORS
+//        int b7 = 4;
+//        b7 -= 3; // ASSIGNMENT OPERATORS
+//        int b8 = 6;
+//        b8 *= 3; // ASSIGNMENT OPERATORS
+//        System.out.println(b);
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        System.out.println(b3);
+//        System.out.println(b4);
+//        System.out.println(b7);
+//        System.out.println(b8);
+
+//        COMPARISON OPERATOR
+
+        System.out.println(67>8);
+
+//        LOGICAL OPERATORS
+
+        System.out.println(67>9 && 98<99);
+        System.out.println(99>6 || 78>89);
+    }
+}
